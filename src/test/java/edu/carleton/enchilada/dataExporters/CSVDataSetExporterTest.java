fa8bac09a98684cbe1138fc94ce02c01c4d8595e
Dave Musicant (home machine) <dmusicant@carleton.edu>
2020-08-10 13:56:11 -0500

    a test passed

--- src/test/java/edu/carleton/enchilada/dataExporters/CSVDataSetExporterTest.java
+++ src/test/java/edu/carleton/enchilada/dataExporters/CSVDataSetExporterTest.java
@@ -49,6 +49,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import javax.swing.JFrame;
 
@@ -212,18 +213,34 @@ public class CSVDataSetExporterTest extends TestCase {
 		assertEquals(null, reader.readLine());
 	}
 
+	// Make sure works for multiple collections
 	public void testExportHistogramToCSV() throws IOException, SQLException {
 
+		File tempFile = File.createTempFile("test", ".csv");
+		System.out.println(tempFile.toString());
+		ArrayList<Double> bins = new ArrayList<>();
+		for (int bin = 1; bin <= 50; bin++) {
+			bins.add((double)bin);
+		}
 		exporter.exportHistogramToCSV(
 				new Collection[]{db.getCollection(2)},
-				File.createTempFile("test", ".csv").toString(),
+				tempFile.toString(),
 				"height sum",
 				"",
 				"",
-				10,
+				1,
 				"",
-				new ArrayList<>());
+				bins);
 
+		try (Scanner scanner = new Scanner(tempFile)) {
+			assertEquals(
+					"Date,StartTime,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50",
+					scanner.next());
+			assertEquals(
+					"2003-09-02,17:30:38,0.0,0.0,0.0,0.0,0.0,12.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,12.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,36.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,12.0,0.0,0.0,0.0,0.0,0.0",
+					scanner.next());
+			assertFalse(scanner.hasNext());
+		}
 
 	}
 
